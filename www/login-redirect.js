1fa0d3f7e5d80bd67fcb8b3243997bd358310691
Ernest Gu <ernest.gu.3.14@gmail.com>
2023-07-18 06:21:28 -0700

    Rewrite login redirect script to use token-based API
    and also to handle error messages

--- www/login-redirect.js
+++ www/login-redirect.js
@@ -1,13 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-	const key_loggedIn = "isLoggedIn";
-	const key_authState = "discordAuthState";
+document.addEventListener("DOMContentLoaded", async () => {
+	const key_authState  = "discordAuthState";
 	const key_authResume = "discordAuthResume";
-	const key_code = "discordCode";
-	const key_name = "discordName";
-	const key_icon = "discordIcon";
+	const key_token  = "guildToken";
+	const key_expiry = "guildExpiry";
+	const key_name   = "guildName";
+	const key_icon   = "guildIcon";
+	const key_rank   = "guildRank";
+
+	function RemoveLoginData() {
+		sessionStorage.removeItem(key_token );
+		sessionStorage.removeItem(key_expiry);
+		sessionStorage.removeItem(key_name  );
+		sessionStorage.removeItem(key_icon  );
+		sessionStorage.removeItem(key_rank  );
+	}
 
 	// Parse returned data.
-	const response = new URLSearchParams(window.location.search);
+	const responseDiscord = new URLSearchParams(window.location.search);
 
 	// Figure out where to redirect to.
 	// If unknown, redirect to homepage.
@@ -20,25 +29,60 @@ document.addEventListener("DOMContentLoaded", () => {
 	let didAuthSucceed = true;
 	
 	const state = sessionStorage.getItem(key_authState);
-	if (state === null || state != response.get("state")) {
+	if (state === null || state != responseDiscord.get("state")) {
 		didAuthSucceed = false;
 	}
 
-	if (response.has("error")) {
+	if (responseDiscord.has("error")) {
 		didAuthSucceed = false;
 	}
 
-	// Set session storage based on authentication validity,
+	// Redirect immediately if authentication wasn't even valid.
+	// No need to show login screen here because either the login was cancelled,
+	// forged, or there was an attempted CSRF (not initiated by the user).
 	if (!didAuthSucceed) {
-		sessionStorage.removeItem(key_loggedIn);
-		sessionStorage.removeItem(key_code);
-		sessionStorage.removeItem(key_name);
-		sessionStorage.removeItem(key_icon);
-	} else {
-		sessionStorage.setItem(key_loggedIn, "true");
-		sessionStorage.setItem(key_code, response.get("code"));
+		RemoveLoginData();
+		window.location.href = redirect;
 	}
 
-	// Redirect based on previously-determined location.
-	window.location.href = redirect;
+	// Log in and fetch a valid token.
+	// (Actually populating the rest of the data happens on page load.)
+	const endpoint = "https://api.erythro.org/irene/web-auth/login";
+	const oauthCode = responseDiscord.get("code");
+	const request = new Request(endpoint, {
+		method: "POST",
+		body: oauthCode,
+	});
+	const responseIrene = await fetch(request);
+
+	// Assume request succeeds--handling timeouts is non-trivial and would
+	// require a better way to surface error messages to the user.
+	if (!responseIrene.ok) {
+		window.location.href = redirect;
+	}
+	const loginData = await responseIrene.body.json();
+
+	// Depending on if login succeeded, either display an error message
+	// or parse the response and redirect back to the resume page.
+	if (!loginData["isUserUnknown"]) {
+		sessionStorage.setItem(key_token , loginData[key_token ]);
+		sessionStorage.setItem(key_expiry, loginData[key_expiry]);
+		window.location.href = redirect;
+	} else {
+		document.getElementById("discord-banner")
+			.setAttribute("src", loginData["discordBanner"]);
+		document.getElementById("discord-pfp")
+			.setAttribute("src", loginData["discordPfp"]);
+		document.getElementById("discord-displayname")
+			.innerText = loginData["discordDisplayname"];
+		document.getElementById("discord-handle")
+			.innerText = loginData["discordHandle"];
+
+		document.getElementById("error-action-resume")
+			.setAttribute("href", )
+
+		// Display error message once all fields have been populated.
+		const node_main = document.querySelector("main");
+		node_main.removeAttribute("hidden");
+	}
 });
